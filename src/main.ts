89bb4a40ba7e80a2dadaed46628c9292ca99f4aa
RoDDy18 <damilolaoni2004@gmail.com>
2025-01-26 14:28:17 +0100

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import { AppModule } from "./app.module";
 import * as express from "express";
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from "@nestjs/common";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: true, credentials: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS" });
+  app.use(helmet());
+  
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
   app.use(cookieParser());
